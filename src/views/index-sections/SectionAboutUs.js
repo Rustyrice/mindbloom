3091036568bb6826bd8359cb68f5639bd7157fe2
Rustyrice <89965863+Rustyrice@users.noreply.github.com>
2023-02-17 22:17:57 +0000

    update on about us section

--- src/views/index-sections/SectionAboutUs.js
+++ src/views/index-sections/SectionAboutUs.js
@@ -32,7 +32,58 @@ function SectionAboutUs() {
                         <CardBody>
                             <a href="#pablo" onClick={(e) => e.preventDefault()}>
                             <div className="author">
-                                <CardTitle tag="h4">Henry Ford</CardTitle>
+                                <CardTitle tag="h4">Chris Jones</CardTitle>
+                                <h6 className="card-category">Back-end developer</h6>
+                            </div>
+                            </a>
+                            <p className="card-description text-center">
+                            Teamwork is so important that it is virtually impossible
+                            for you to reach the heights of your capabilities or make
+                            the money that you want without becoming very good at it.
+                            </p>
+                        </CardBody>
+                        <CardFooter className="text-center">
+                            <Button
+                            className="btn-just-icon btn-neutral"
+                            color="link"
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                            >
+                            <i className="fa fa-twitter" />
+                            </Button>
+                            <Button
+                            className="btn-just-icon btn-neutral ml-1"
+                            color="link"
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                            >
+                            <i className="fa fa-google-plus" />
+                            </Button>
+                            <Button
+                            className="btn-just-icon btn-neutral ml-1"
+                            color="link"
+                            href="#pablo"
+                            onClick={(e) => e.preventDefault()}
+                            >
+                            <i className="fa fa-linkedin" />
+                            </Button>
+                        </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col md="4">
+                        <Card className="card-profile card-plain">
+                        <div className="card-avatar">
+                            <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                            <img
+                                alt="..."
+                                src={require("assets/img/default-avatar.png")}
+                            />
+                            </a>
+                        </div>
+                        <CardBody>
+                            <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                            <div className="author">
+                                <CardTitle tag="h4">Varniethan Ketheeswaran</CardTitle>
                                 <h6 className="card-category">Front-end developer</h6>
                             </div>
                             </a>
@@ -83,59 +134,8 @@ function SectionAboutUs() {
                         <CardBody>
                             <a href="#pablo" onClick={(e) => e.preventDefault()}>
                             <div className="author">
-                                <CardTitle tag="h4">Henry Ford</CardTitle>
-                                <h6 className="card-category">Product Manager</h6>
-                            </div>
-                            </a>
-                            <p className="card-description text-center">
-                            Teamwork is so important that it is virtually impossible
-                            for you to reach the heights of your capabilities or make
-                            the money that you want without becoming very good at it.
-                            </p>
-                        </CardBody>
-                        <CardFooter className="text-center">
-                            <Button
-                            className="btn-just-icon btn-neutral"
-                            color="link"
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                            >
-                            <i className="fa fa-twitter" />
-                            </Button>
-                            <Button
-                            className="btn-just-icon btn-neutral ml-1"
-                            color="link"
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                            >
-                            <i className="fa fa-google-plus" />
-                            </Button>
-                            <Button
-                            className="btn-just-icon btn-neutral ml-1"
-                            color="link"
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                            >
-                            <i className="fa fa-linkedin" />
-                            </Button>
-                        </CardFooter>
-                        </Card>
-                    </Col>
-                    <Col md="4">
-                        <Card className="card-profile card-plain">
-                        <div className="card-avatar">
-                            <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                            <img
-                                alt="..."
-                                src={require("assets/img/default-avatar.png")}
-                            />
-                            </a>
-                        </div>
-                        <CardBody>
-                            <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                            <div className="author">
-                                <CardTitle tag="h4">Henry Ford</CardTitle>
-                                <h6 className="card-category">Product Manager</h6>
+                                <CardTitle tag="h4">Oliwer Kolodziejczyk</CardTitle>
+                                <h6 className="card-category">Back-end developer</h6>
                             </div>
                             </a>
                             <p className="card-description text-center">
@@ -178,15 +178,15 @@ function SectionAboutUs() {
                             <a href="#pablo" onClick={(e) => e.preventDefault()}>
                             <img
                                 alt="..."
-                                src={require("assets/img/faces/joe-gardner-2.jpg")}
+                                src={require("assets/img/default-avatar.png")}
                             />
                             </a>
                         </div>
                         <CardBody>
                             <a href="#pablo" onClick={(e) => e.preventDefault()}>
                             <div className="author">
-                                <CardTitle tag="h4">Sophie West</CardTitle>
-                                <h6 className="card-category">Designer</h6>
+                                <CardTitle tag="h4">Maciej Kosela</CardTitle>
+                                <h6 className="card-category">Front-end developer</h6>
                             </div>
                             </a>
                             <p className="card-description text-center">
@@ -230,15 +230,15 @@ function SectionAboutUs() {
                             <a href="#pablo" onClick={(e) => e.preventDefault()}>
                             <img
                                 alt="..."
-                                src={require("assets/img/faces/erik-lucatero-2.jpg")}
+                                src={require("assets/img/default-avatar.png")}
                             />
                             </a>
                         </div>
                         <CardBody>
                             <a href="#pablo" onClick={(e) => e.preventDefault()}>
                             <div className="author">
-                                <CardTitle tag="h4">Robert Orben</CardTitle>
-                                <h6 className="card-category">Developer</h6>
+                                <CardTitle tag="h4">Nathan Lim</CardTitle>
+                                <h6 className="card-category">Back-end Developer</h6>
                             </div>
                             </a>
                             <p className="card-description text-center">
