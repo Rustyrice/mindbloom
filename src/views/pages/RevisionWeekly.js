b9221148dd1ec2d1adedd348103d956270eefd85
chrisjones243 <38958680+chrisjones243@users.noreply.github.com>
2023-03-09 11:34:07 +0000

    revision update weekly page change

--- src/views/pages/RevisionWeekly.js
+++ src/views/pages/RevisionWeekly.js
@@ -21,7 +21,7 @@ import {
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import LandingPageHeader from "components/Headers/LandingPageHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
-import IndexHeader from "components/Headers/IndexHeader.js";
+import IndexNavbar from "components/Navbars/IndexNavbar";
 
 
 function RevisionWeeklyPage(){
@@ -34,92 +34,7 @@ function RevisionWeeklyPage(){
     });
     return (
       <>
-        <table class="tg">
-        <thead>
-        <tr>
-            <th class="tg-0lax"></th>
-            <th class="tg-0lax">Monday</th>
-            <th class="tg-0lax">Tuesday</th>
-            <th class="tg-0lax">Wednesday</th>
-            <th class="tg-0lax">Thursday</th>
-            <th class="tg-0lax">Friday</th>
-            <th class="tg-0lax">Saturday</th>
-            <th class="tg-0lax">Sunday</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td class="tg-0lax">Pomodoro1</td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-        </tr>
-        <tr>
-            <td class="tg-0lax"><span style="font-weight:400;font-style:normal;text-decoration:none">Pomodoro2</span></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-        </tr>
-        <tr>
-            <td class="tg-0lax"><span style="font-weight:400;font-style:normal;text-decoration:none">Pomodoro3</span></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-        </tr>
-        <tr>
-            <td class="tg-0lax"><span style="font-weight:400;font-style:normal;text-decoration:none">Pomodoro4</span></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-        </tr>
-        <tr>
-            <td class="tg-0lax"><span style="font-weight:400;font-style:normal;text-decoration:none">Pomodoro5</span></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-        </tr>
-        <tr>
-            <td class="tg-0lax"><span style="font-weight:400;font-style:normal;text-decoration:none">Pomodoro6</span></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-        </tr>
-        <tr>
-            <td class="tg-0lax"><span style="font-weight:400;font-style:normal;text-decoration:none">Pomodoro7</span></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-            <td class="tg-0lax"></td>
-        </tr>
-        </tbody>
-        </table>
+        <IndexNavbar />
       </>
     
     );
