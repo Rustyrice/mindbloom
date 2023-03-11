6519e3d2585d80c3a7d6e85cd776eef292453bc5
Rustyrice <89965863+Rustyrice@users.noreply.github.com>
2023-03-11 23:31:12 +0000

    pomodoro timer not functioning as expected
    need help on the javascript for the pomodoro timer. it is not functioning as expected

--- src/views/pages/RevisionDailyNew.js
+++ src/views/pages/RevisionDailyNew.js
@@ -8,79 +8,90 @@ import {
     Input,
     Container,
     Button
-  } from "reactstrap";
+} from "reactstrap";
 
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar";
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import LandingPageHeader from "components/Headers/LandingPageHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
-
 import { ListItem } from "components/ListGroupItem";
-
 
 
 function RevisionDailyPage() {
     const Ref = useRef(null);
-
     const [timer, setTimer] = useState('00:00:00');
+    const [isRunning, setIsRunning] = useState(false);
+    const [endTime, setEndTime] = useState(null);
 
-    const getTimeRemaining = (e) => {
-    const total = Date.parse(e) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-    return {
-        total, hours, minutes, seconds
-    }
-
-    }
-
-    const startTimer = (e) => {
-        let { total, hours, minutes, seconds } 
-                    = getTimeRemaining(e);
-        if (total >= 0) {
-
+    const getTimeRemaining = (endTime) => {
+        const total = Date.parse(endTime) - Date.parse(new Date());
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((total / (1000 * 60)) % 60);
+        const seconds = Math.floor((total / 1000) % 60);
+        return {
+          total,
+          hours,
+          minutes,
+          seconds,
+        };
+    };
+    
+    const startTimer = () => {
+        let { total, hours, minutes, seconds } = getTimeRemaining(endTime);
+        if (total >= 0 && timer != '00:00:00') {
             // update the timer
-            // check if less than 10 then we need to 
+            // check if less than 10 then we need to
             // add '0' at the beginning of the variable
             setTimer(
-                (hours > 9 ? hours : '0' + hours) + ':' +
-                (minutes > 9 ? minutes : '0' + minutes) + ':'
-                + (seconds > 9 ? seconds : '0' + seconds)
-            )
+                (hours > 9 ? hours : '0' + hours) +
+                ':' +
+                (minutes > 9 ? minutes : '0' + minutes) +
+                ':' +
+                (seconds > 9 ? seconds : '0' + seconds)
+            );
+            setEndTime(new Date(endTime.getTime() - 1000));
+        } else {
+            // pause the timer
+            setIsRunning(false);
+            if (Ref.current) clearInterval(Ref.current);
+            setTimer('00:00:00');
         }
-    }
-
-    const clearTimer = (e) => {
+    };
     
-        // set the timer to 10 sec
-        setTimer('00:00:10');
-
+    const onClickStartPause = () => {
+        if (!isRunning) {
+            // start the timer
+            const endTime = new Date();
+            endTime.setSeconds(endTime.getSeconds() + 1500);
+            setEndTime(endTime);
+            setIsRunning(true);
+            const id = setInterval(() => {
+                startTimer();
+            }, 1000);
+            Ref.current = id;
+        } else {
+            // pause the timer
+            setIsRunning(false);
+            if (Ref.current) clearInterval(Ref.current);
+            setEndTime(new Date(endTime.getTime() + (new Date() - new Date(endTime))));
+        }
+    };
+    
+    const clearTimer = () => {
+        // set the timer to 25 mins
+        setTimer('00:25:00');
+        const endTime = new Date();
+        endTime.setMinutes(endTime.getMinutes() + 25);
+        endTime.setSeconds(0);
+        setEndTime(endTime);
         // clear the timer
         if (Ref.current) clearInterval(Ref.current);
-        const id = setInterval(() => {
-            startTimer(e);
-        }, 1000)
-        Ref.current = id;
-    }
-
-    const getDeadTime = () => {
-        let deadline = new Date();
-
-        // This is where you need to adjust if 
-        // you entend to add more time
-        deadline.setSeconds(deadline.getSeconds() + 10);
-        return deadline;
-    }
-
+    };
+    
     useEffect(() => {
-        clearTimer(getDeadTime());
+        clearTimer();
     }, []);
-
-    const onClickReset = () => {
-        clearTimer(getDeadTime());
-    }
 
   document.documentElement.classList.remove("nav-open");
   React.useEffect(() => {
@@ -88,8 +99,7 @@ function RevisionDailyPage() {
     return function cleanup() {
       document.body.classList.remove("revision-landing-page");
     };
-  })
-
+  });
   
     return (
       <div>
@@ -103,10 +113,10 @@ function RevisionDailyPage() {
             backgroundImage: "url(https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80)",
         }}>
             <div style={{backgroundColor: "white", borderRadius: "5px", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", padding: "0px 50px 10px 50px"}}>
-                <h1 style={{marginBottom: "10px"}}>25:00</h1>
-                <Button color="danger" onClick={onClickReset}>Reset</Button>
-            </div>
+                <h1 style={{marginBottom: "10px"}}>{timer}</h1>
+                <Button color="danger" onClick={onClickStartPause}>{isRunning ? 'Pause' : 'Start'}</Button>
 
+            </div>
 
         </div>
           <div className="content-center" style={{paddingTop: "30px"}}>
@@ -144,7 +154,7 @@ function RevisionDailyPage() {
                 </ListGroup>
             </Container>
           </div>
-      </div>
+        </div>
       
     );
 }
