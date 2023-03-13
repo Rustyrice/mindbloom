a479a2e8c1e4b9fee4d04bf06e5bb8662d489c49
chrisjones243 <38958680+chrisjones243@users.noreply.github.com>
2023-03-13 19:55:17 +0000

    Some dashboard UI change

--- src/views/pages/DashboardPage.js
+++ src/views/pages/DashboardPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 // reactstrap components
-import { Container } from "reactstrap";
+import { Container, Button } from "reactstrap";
 
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar";
@@ -11,6 +12,8 @@ import { supabase } from "config/client";
 
 function DashboardPage() {
   const [user, setUser] = useState(null);
+
+  let history = useHistory();
 
   useEffect(() => {
     supabase.auth.getUser().then((value) => {
@@ -33,10 +36,30 @@ function DashboardPage() {
   return (
     <>
       <IndexNavbar />
-      <ProfilePageHeader />
-      <div className="section profile-content">
+      <div style={{
+            display: "flex",
+            // justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "30vh",
+            
+            backgroundImage: "url(https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80)",
+        }}>
         <Container>
-          <div className="owner">
+          <h1 style={{fontWeight: "bold", color: "white"}}>Hi {user?.user_metadata.name}</h1>
+        </Container>
+
+        </div>
+      <div className="section profile-content">
+        <Container style={{marginTop: "10px"}}>
+          {/* Usefull links */}
+          <Button style={{marginRight: "10px"}} onClick={() => history.push("revision-landing-page")}> Revision Page </Button>
+          {/* <Button onClick={() => history.push("dashboard")}> Dashboard Page </Button> */}
+          <br />
+          <br />
+          <p>This page will show all the data and trends about Personal Informatics according to user Input</p>
+
+          {/* <div className="owner">
             <div className="avatar">
               <img
                 alt="..."
@@ -50,7 +73,7 @@ function DashboardPage() {
               </h4>
               <p>This page will show all the data and trends about Personal Informatics according to user Input</p>
             </div>
-          </div>
+          </div> */}
           
         </Container>
       </div>
