f460bc321ec864685aae10d5ad17e61953224c28
chrisjones243 <38958680+chrisjones243@users.noreply.github.com>
2023-04-15 19:23:41 +0100

    Some corrections on the badge titles

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -5,9 +5,9 @@ function Badges({ points }) {
 
   return (
     <div style={{width: "100%",display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gridTemplateRows: "1fr 1fr", gridGap: "10px"}}>
-        <GiRank1 size="4em" style={{justifySelf: "center"}} title="You earn this badge by completing 300 tasks!"/>
-        <GiRank2 size="4em" style={{justifySelf: "center"}} title="You earn this badge by completing 1,000 tasks!"/>
-        <GiRank3 size="4em" style={{justifySelf: "center"}} title="You earn this badge by completing 10,000 tasks!"/>
+        <GiRank1 size="4em" style={{justifySelf: "center"}} title="You earn this badge by having 300 points!"/>
+        <GiRank2 size="4em" style={{justifySelf: "center"}} title="You earn this badge by having 1,000 points!"/>
+        <GiRank3 size="4em" style={{justifySelf: "center"}} title="You earn this badge by having 10,000 points!"/>
 
         <h3 style={{justifySelf: "center"}}>{parseInt(points / 300)}</h3>
         <h3 style={{justifySelf: "center"}}>{parseInt(points / 1000)}</h3>
